adf3281d47b1ef9221d9108c471fe4694bd80e3e
Shuaiyi Xu <305591729@qq.com>
2018-04-17 04:20:46 -0400

    install socketio and localforage

--- src/App.js
+++ src/App.js
@@ -5,8 +5,56 @@ import MainChatView from "./MainChatView";
 import MainFriendsView from "./MainFriendsView"
 import { bindActionCreators } from 'redux';
 import { connect } from "react-redux";
+import openSocket from 'socket.io-client';
+import localforage from 'localforage';
+
+const socket = openSocket('http://47.89.187.42:4000');
+const uid = "pbc6cqcW80XHRbBJptIcqEVZpjY2";
+
 
 class App extends Component {
+    componentDidMount(){
+        localforage.config({
+            driver      : localforage.INDEXEDDB,
+            name        : 'Tinko',
+            version     : 1.0,
+            size        : 4980736,
+            storeName   : 'TinkoChat',
+            description : 'This is the chat database'
+        });
+        socket.on('connect'+uid, data => this.getChatData(data));
+    }
+    getChatData(d){
+        console.log(d);
+        let data = JSON.parse(d);
+        if (data.type === 1){
+            //{"type":1,"from":"mrhUd8E7YNg3ZWW4EdEK5hIZfVE2","message":"ss","time":1523903819000}
+            //private chat
+            let fromId = data.from;
+            localforage.getItem(fromId).then(function(value) {
+                let rtn = [],
+                    jsonData = JSON.stringify({
+                        message:data.message,
+                        time:data.time
+                    });
+                if (value === null){
+                    rtn = [jsonData];
+                }else{
+                    rtn = value;
+                    rtn.push(jsonData);
+                }
+                localforage.setItem(fromId, rtn).then(function (value) {
+                    console.log("insert chat data success");
+                }).catch(function(err) {
+                    console.log(err);
+                });
+            }).catch(function(err) {
+                // This code runs if there were any errors
+                console.log(err);
+            });
+        }
+    }
+
     render() {
         if (!this.props.activePage){
             return (
